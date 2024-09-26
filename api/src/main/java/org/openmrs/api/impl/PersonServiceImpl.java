9c785254ef305921ce8043081711e09b502638bd
ZaninaMehdi <mehdi.zanina@polymtl.ca>
2024-09-26 11:28:22 -0400

    missing import

--- api/src/main/java/org/openmrs/api/impl/PersonServiceImpl.java
+++ api/src/main/java/org/openmrs/api/impl/PersonServiceImpl.java
@@ -33,6 +33,7 @@ import org.openmrs.validator.ValidateUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.slf4j.MarkerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.Assert;
 
